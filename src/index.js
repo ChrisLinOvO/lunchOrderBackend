9cb31e53978277299b9a12c40f9cf92d61c6e49e
DGX Spark <spark@dgx-spark.local>
2026-04-10 10:36:41 +0800

    fix: use app.listen().then() pattern for Feathers v5

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 const logger = require('./logger')
 const app = require('./app')
 const port = app.get('port')
-const server = app.listen(port)
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 )
 
-server.on('listening', () =>
+app.listen(port).then(server => {
   logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-)
+}).catch(err => {
+  logger.error('Failed to start server:', err)
+  process.exit(1)
+})
